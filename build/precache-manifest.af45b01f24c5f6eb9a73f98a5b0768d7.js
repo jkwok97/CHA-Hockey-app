e0e5564c52dcb2ac02ed0a6f7bbd5955214cbe70
Jeff Kwok <43362218+jkwok97@users.noreply.github.com>
2018-11-28 22:41:26 -0700

    updated css to include scaling of frames

---
+++ build/precache-manifest.af45b01f24c5f6eb9a73f98a5b0768d7.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "715e5df68a099bb20aed5a7476e66a43",
+    "url": "/CHA-Hockey-app/static/media/Hound4.715e5df6.jpg"
+  },
+  {
+    "revision": "68887c3393298b095996",
+    "url": "/CHA-Hockey-app/static/css/main.a8a64411.chunk.css"
+  },
+  {
+    "revision": "6debcf90a2bd3d418c77",
+    "url": "/CHA-Hockey-app/static/js/1.6debcf90.chunk.js"
+  },
+  {
+    "revision": "2c87a61cb6c29c848ea6",
+    "url": "/CHA-Hockey-app/static/js/runtime~main.2c87a61c.js"
+  },
+  {
+    "revision": "9b7d46b7bec428305f30817f9a4c1ad9",
+    "url": "/CHA-Hockey-app/static/media/High+Res+Hockey+5.9b7d46b7.jpg"
+  },
+  {
+    "revision": "332ee52cdb2edc6ed224d95cf401a0ff",
+    "url": "/CHA-Hockey-app/static/media/cha_logo.332ee52c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CHA-Hockey-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0e11ba03f5bb0bb302a457fce83c7d29",
+    "url": "/CHA-Hockey-app/static/media/assassins.0e11ba03.jpg"
+  },
+  {
+    "revision": "e77b0402deab16e70f9619aec9940bc9",
+    "url": "/CHA-Hockey-app/static/media/Mountaineers6.e77b0402.jpg"
+  },
+  {
+    "revision": "2d8388ef3e97e6d287cf7da244279402",
+    "url": "/CHA-Hockey-app/static/media/cocks2.2d8388ef.jpg"
+  },
+  {
+    "revision": "fd3e6c6e004acd1757dbefe5495dbceb",
+    "url": "/CHA-Hockey-app/static/media/seattlestorm.fd3e6c6e.jpg"
+  },
+  {
+    "revision": "52c316458e4085ab70ad6b6570b03f2a",
+    "url": "/CHA-Hockey-app/static/media/Vipers.52c31645.jpg"
+  },
+  {
+    "revision": "f747653415d581edd3b0fa54f4b56b86",
+    "url": "/CHA-Hockey-app/static/media/Desperado7.f7476534.jpg"
+  },
+  {
+    "revision": "260a9c650f138ed5d523a766963a5ba8",
+    "url": "/CHA-Hockey-app/static/media/oilbarons3.260a9c65.jpg"
+  },
+  {
+    "revision": "68887c3393298b095996",
+    "url": "/CHA-Hockey-app/static/js/main.68887c33.chunk.js"
+  },
+  {
+    "revision": "68735f726df67673e1599a868e5369cb",
+    "url": "/CHA-Hockey-app/static/media/wolfpack.68735f72.jpg"
+  },
+  {
+    "revision": "05d23aedc4489c8f2b57710f5f2967b0",
+    "url": "/CHA-Hockey-app/static/media/Brahmas3.05d23aed.jpg"
+  },
+  {
+    "revision": "7d423b7cb5725e3c048ad330a08661b1",
+    "url": "/CHA-Hockey-app/static/media/dragz.7d423b7c.jpg"
+  },
+  {
+    "revision": "36f2873841657e76ee534942b04a95ef",
+    "url": "/CHA-Hockey-app/static/media/Goats11.36f28738.jpg"
+  },
+  {
+    "revision": "5a09fe1e112e2ded0197a3707824d002",
+    "url": "/CHA-Hockey-app/static/media/KillerBees.5a09fe1e.jpg"
+  },
+  {
+    "revision": "70070166d5dca853a344c974b9ad258b",
+    "url": "/CHA-Hockey-app/static/media/glory2.70070166.jpg"
+  },
+  {
+    "revision": "f39a1c65832d4dd7ebf93dd5446b9de4",
+    "url": "/CHA-Hockey-app/static/media/Prowlers2.f39a1c65.jpg"
+  },
+  {
+    "revision": "9ec20b1c758dcd2b4797f60fa1fd1b01",
+    "url": "/CHA-Hockey-app/static/media/Cyclones4.9ec20b1c.jpg"
+  },
+  {
+    "revision": "a1540e302704456a83f00b8f460b14a4",
+    "url": "/CHA-Hockey-app/static/media/Flashers2.a1540e30.jpg"
+  },
+  {
+    "revision": "1fb337bcbce0865802dd5d849e829abd",
+    "url": "/CHA-Hockey-app/static/media/rays.1fb337bc.jpg"
+  },
+  {
+    "revision": "577470e5259499982b979640f294e57c",
+    "url": "/CHA-Hockey-app/static/media/voodoo2.577470e5.jpg"
+  },
+  {
+    "revision": "84cb3ffe6fa8254b24d14b4452d8f221",
+    "url": "/CHA-Hockey-app/static/media/Green Jackets.84cb3ffe.jpg"
+  },
+  {
+    "revision": "9b4a6c3c83c77263b9c6ddef269b2267",
+    "url": "/CHA-Hockey-app/index.html"
+  }
+];
